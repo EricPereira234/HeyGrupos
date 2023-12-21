ef9da11736ab8b188b059065f34a9581b3befc21
Eric Pereira <ericpereira234@gmail.com>
2023-12-21 07:55:19 -0300

    iniciando estrutura do projeto

---
+++ heygrupos/src/pages/ChatRoom/index.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default function ChatRoom(){
+    return(
+        <h1>Tela chatroom</h1>
+    )
+}
---
+++ heygrupos/src/pages/Messages/index.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Messages(){
+    return(
+        <h1>tela de mensagens</h1>
+    )
+}
