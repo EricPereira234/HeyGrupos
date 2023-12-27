3aacda23e7d36c259f82806d09edc4da22ca35b7
Eric Pereira <ericpereira234@gmail.com>
2023-12-27 07:53:38 -0300

    criando a funcionalidade de criar user

--- heygrupos/src/pages/SiginUp/index.tsx
+++ heygrupos/src/pages/SiginUp/index.tsx
@@ -1,16 +1,32 @@
 import styles from './siginup.module.css';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SiginUp() {
+    const [nome, setNome] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    function handleCreateUser(e:any){
+        e.preventDefault();
+        alert('teste');
+    }
+
     return (
         <div className={styles.cardUp} >
             <div className={styles.cardInputs} >
-                <form className={styles.fromLogin} >
+                <form className={styles.fromLogin} onSubmit={handleCreateUser} >
                     <label className={styles.titulnoLogi} >HeyGrupos</label>
-                    <input type="text" placeholder="seu nome..."  className={styles.inputsLogin}  />
-                    <input type="email" placeholder="exemple@email.com" className={styles.inputsLogin} />
-                    <input type="password" placeholder="*******"  className={styles.inputsLogin} />
-                    <input type="submit" value={'Salvar'} className={styles.buttonLogin} />
+                    <input type="text" placeholder="seu nome..." className={styles.inputsLogin}
+                        onChange={(e) => setNome(e.target.value)}
+                    />
+                    <input type="email" placeholder="exemple@email.com" className={styles.inputsLogin}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input type="password" placeholder="*******" className={styles.inputsLogin} />
+                    <input type="submit" value={'Salvar'} className={styles.buttonLogin}
+                        onChange={(e) => setNome(e.target.value)}
+                    />
                     <Link to={'/login'} >faça login</Link>
                 </form>
             </div>
