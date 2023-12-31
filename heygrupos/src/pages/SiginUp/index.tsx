d75321ce1ed85889feece7bd19d70aa3e70474d5
Eric Pereira <ericpereira234@gmail.com>
2023-12-31 11:42:23 -0300

    criando função logar e sinUp

--- heygrupos/src/pages/SiginUp/index.tsx
+++ heygrupos/src/pages/SiginUp/index.tsx
@@ -2,34 +2,67 @@ import styles from './siginup.module.css';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+import { db, auth } from '../../services/firebaseConection';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+
 export default function SiginUp() {
-    const [nome, setNome] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+  const [nome, setNome] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-    function handleCreateUser(e:any){
-        e.preventDefault();
-        alert('teste');
+  const navigate = useNavigate();
+
+  async function handleCreateUser(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (nome.trim() !== '' && email.trim() !== '' && password.trim() !== '') {
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const uid = userCredential.user.uid;
+
+        // Cria um documento para o usuário no Firestore
+        const userDocRef = doc(db, 'users', uid);
+        await setDoc(userDocRef, { nome, email });
+
+        alert('Cadastrado com sucesso!');
+        navigate('/login'); 
+      } catch (error) {
+        console.error('Erro ao criar usuário: '+ error );
+        alert('Erro ao criar usuário. Verifique a console para mais detalhes.');
+      }
+    } else {
+      alert('Verifique todos os campos!');
     }
+  }
 
-    return (
-        <div className={styles.cardUp} >
-            <div className={styles.cardInputs} >
-                <form className={styles.fromLogin} onSubmit={handleCreateUser} >
-                    <label className={styles.titulnoLogi} >HeyGrupos</label>
-                    <input type="text" placeholder="seu nome..." className={styles.inputsLogin}
-                        onChange={(e) => setNome(e.target.value)}
-                    />
-                    <input type="email" placeholder="exemple@email.com" className={styles.inputsLogin}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input type="password" placeholder="*******" className={styles.inputsLogin} />
-                    <input type="submit" value={'Salvar'} className={styles.buttonLogin}
-                        onChange={(e) => setNome(e.target.value)}
-                    />
-                    <Link to={'/login'} >faça login</Link>
-                </form>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.cardUp}>
+      <div className={styles.cardInputs}>
+        <form className={styles.fromLogin} onSubmit={handleCreateUser}>
+          <label className={styles.titulnoLogi}>HeyGrupos</label>
+          <input
+            type="text"
+            placeholder="Seu nome..."
+            className={styles.inputsLogin}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Exemplo@email.com"
+            className={styles.inputsLogin}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="*******"
+            className={styles.inputsLogin}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input type="submit" value={'Cadastrar'} className={styles.buttonLogin} />
+          <Link to={'/login'}>Faça login</Link>
+        </form>
+      </div>
+    </div>
+  );
 }
--- heygrupos/src/pages/Signin/index.tsx
+++ heygrupos/src/pages/Signin/index.tsx
@@ -1,18 +1,65 @@
 import styles from "./login.module.css";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Login(){
-    return(
-        <div className={styles.cardLogin} >
-          <div className={styles.cardInputs} >
-            <form className={styles.fromLogin} >
-                <label className={styles.tituloLogin} >HeyGrupos</label>
-                <input type="email" placeholder="exemple@email.com" className={styles.inputsLogin} />
-                <input type="password" placeholder="*******" className={styles.inputsLogin} />
-                <input type="submit" value={'Salvar'} className={styles.buttonLogin}  />
-                <Link to={'/siginup'} >criar conta</Link>
-            </form>
-          </div>
-        </div>
-    )
+import { auth, db } from '../../services/firebaseConection';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+
+export default function Login() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const navigate = useNavigate();
+
+  async function SingIne(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const uid = userCredential.user.uid;
+      const docRef = doc(db, "users", uid);
+      const docSnap = await getDoc(docRef);
+
+      const userData = {
+        uid: uid,
+        nome: docSnap.data()?.nome,
+        email: userCredential.user.email
+      };
+
+      // Armazena os dados do usuário no localStorage
+      localStorage.setItem('@heygrupos', JSON.stringify(userData));
+
+      console.log('Usuário logado com sucesso:', userData);
+      
+      // Redireciona para a página desejada após o login
+      navigate('/');
+    } catch (error) {
+      console.error('Erro ao fazer login:');
+      alert('Erro ao fazer login. Verifique as credenciais e tente novamente.');
+    }
+  }
+
+  return (
+    <div className={styles.cardLogin}>
+      <div className={styles.cardInputs}>
+        <form className={styles.fromLogin} onSubmit={SingIne}>
+          <label className={styles.tituloLogin}>HeyGrupos</label>
+          <input
+            type="email"
+            placeholder="exemplo@email.com"
+            className={styles.inputsLogin}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="*******"
+            className={styles.inputsLogin}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className={styles.buttonLogin}>Entrar</button>
+          <Link to={'/siginup'}>Criar conta</Link>
+        </form>
+      </div>
+    </div>
+  );
 }
